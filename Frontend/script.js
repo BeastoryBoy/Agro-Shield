79df01d34a3e17acca11d206ca8b0b8ccae3bd62
Awaiz Baig <140795956+BeastoryBoy@users.noreply.github.com>
2026-04-24 04:30:15 +0530

    script.js

--- Frontend/script.js
+++ Frontend/script.js
@@ -233,7 +233,7 @@ async function runScan() {
     const formData = new FormData();
     formData.append("image", file);
 
-    const res = await fetch("https://agro-backend.onrender.com/submit", {
+    const res = await fetch("https://agro-backend-aqwn.onrender.com/submit", {
       method: "POST",
       body: formData
     });
